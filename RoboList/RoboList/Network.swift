00aa2a8d1631f2a0f70e32cc9809dac502019823
Mykola Denysyuk <theshark.abc@gmail.com>
2018-06-18 01:37:53 +0300

    Make Network manager fetch all images at the same time

--- RoboList/RoboList/Network.swift
+++ RoboList/RoboList/Network.swift
@@ -10,40 +10,40 @@ import Foundation
 import UIKit
 
 protocol RobotRepository {
-  func fetchFewRobots(names: [String], completion: @escaping ([UIImage]) -> Void)
-  func fetchRobot(with name: String, completion: @escaping (UIImage) -> Void)
+    func fetchFewRobots(names: [String], completion: @escaping ([UIImage]) -> Void)
+    func fetchRobot(with name: String, completion: @escaping (UIImage) -> Void)
 }
 
 class Network: RobotRepository {
-  
-  // TODO: change this function so it will fetch all images at the same time, not one-by-one
-  // but completion should be called only after all images are fetched.
-  // Fell free to change whole class as long as it conforms RobotRepository protocol.
-  func fetchFewRobots(names: [String], completion: @escaping ([UIImage]) -> Void) {
-    var index = 0
-    var images = [UIImage]()
-    func fetchRobot() {
-      self.fetchRobot(with: names[index]) { (image) in
-        images.append(image)
-        index += 1
-        if index < names.count {
-          fetchRobot()
-        } else {
-          completion(images)
+    
+    func fetchFewRobots(names: [String], completion: @escaping ([UIImage]) -> Void) {
+        let updateQueue = DispatchQueue(label: "fetchFewRobots.updateQueue")
+        
+        var images = [UIImage]()
+        let group = DispatchGroup()
+        
+        names.forEach { name in
+            group.enter()
+            fetchRobot(with: name, completion: { image in
+                updateQueue.async {
+                    images.append(image)
+                    group.leave()
+                }
+            })
         }
-      }
+        
+        group.notify(queue: updateQueue) {
+            completion(images)
+        }
     }
     
-    fetchRobot()
-  }
-  
-  // TODO: prevent it from crashing!
-  // propose error handling
-  func fetchRobot(with name: String, completion: @escaping (UIImage) -> Void) {
-    let url = URL(string: "https://robohash.org/\(name)")!
-    let dataTask = URLSession(configuration: .default).dataTask(with: url) { (data, _, _) in
-      completion(UIImage(data: data!)!)
+    // TODO: prevent it from crashing!
+    // propose error handling
+    func fetchRobot(with name: String, completion: @escaping (UIImage) -> Void) {
+        let url = URL(string: "https://robohash.org/\(name)")!
+        let dataTask = URLSession(configuration: .default).dataTask(with: url) { (data, _, _) in
+            completion(UIImage(data: data!)!)
+        }
+        dataTask.resume()
     }
-    dataTask.resume()
-  }
 }
